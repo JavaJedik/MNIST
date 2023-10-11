757c15fe4dce2e4e4bd4570c2e06ead01cd3dc27
Pankotai Kristóf Géza <p.kristof1999@gmail.com>
2023-10-11 16:18:45 +0200

    App.js módosítása, hogy betöltődjenek a további felületek

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,18 @@ import React from 'react';
 import './App.css';
 import Login from './components/login';
 import Home from './components/home';
-//import Register from './components/Register';
-//import Leaderboard from './components/Leaderboard';
+import Register from './components/Register';
+import Leaderboard from './components/Leaderboard';
 import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 function App() {
-  return (
-      <BrowserRouter>
+    return (
+        <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Navigate to="/login"/>}/>
-          <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Navigate to="/login"/>}/>
+            <Route path="/login" element={<Login />} />
             <Route path="/home" element={<Home />} />
-            <!--
             <Route path="/register" element={<Register />} />
             <Route path="/leaderboard" element={<Leaderboard />} />
-            -->
         </Routes>
       </BrowserRouter>
   );
