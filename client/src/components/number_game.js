0ef33af10215b54735d8aae3aab159ca8bbda73d
Pankotai Kristóf Géza <p.kristof1999@gmail.com>
2023-10-17 09:17:57 +0200

    Reszponzivitás a játék oldalon

--- client/src/components/number_game.js
+++ client/src/components/number_game.js
@@ -21,81 +21,85 @@ const Number_Game = () => {
 
     return (
         <div className="main-container">
-
-            <div className="image">
-            </div>
-
-            <div className="game-blur-container"/>
-            <div className="game-container">
-                <div className="content">
-                    <div className="numbers">
-                        <div className="numbers-top">
-                            <div className="numbers-top-text">
-                                Adjon meg egy választ!
+            <div className="game-main-container">
+                <div className="game-left-container">
+                    <div className="image">
+                    </div>
+                </div>
+                <div className="game-right-container">
+                    <div className="game-blur-container"/>
+                    <div className="game-container">
+                        <div className="content">
+                            <div className="numbers">
+                                <div className="numbers-top">
+                                    <div className="numbers-top-text">
+                                        Adjon meg egy választ!
+                                    </div>
+                                </div>
+                                <div className="number" onClick={() => handleClick(1)}>
+                                    <button className="number-button">
+                                        1
+                                    </button>
+                                </div>
+                                <div className="number" onClick={() => handleClick(2)}>
+                                    <button className="number-button">
+                                        2
+                                    </button>
+                                </div>
+                                <div className="number" onClick={() => handleClick(3)}>
+                                    <button className="number-button">
+                                        3
+                                    </button>
+                                </div>
+                                <div className="number" onClick={() => handleClick(4)}>
+                                    <button className="number-button">
+                                        4
+                                    </button>
+                                </div>
+                                <div className="number" onClick={() => handleClick(5)}>
+                                    <button className="number-button">
+                                        5
+                                    </button>
+                                </div>
+                                <div className="number" onClick={() => handleClick(6)}>
+                                    <button className="number-button">
+                                        6
+                                    </button>
+                                </div>
+                                <div className="number" onClick={() => handleClick(7)}>
+                                    <button className="number-button">
+                                        7
+                                    </button>
+                                </div>
+                                <div className="number" onClick={() => handleClick(8)}>
+                                    <button className="number-button">
+                                        8
+                                    </button>
+                                </div>
+                                <div className="number" onClick={() => handleClick(9)}>
+                                    <button className="number-button">
+                                        9
+                                    </button>
+                                </div>
+                                <div className="number"  onClick={() => handleClick(1)}>
+                                    <button className="number-button">
+                                        0
+                                    </button>
+                                </div>
+                                <div className="idk">
+                                    <button className="idk-button" onClick={() => handleClick("Nem tudom")}>
+                                        Nem tudom
+                                    </button>
+                                </div>
                             </div>
-                        </div>
-                        <div className="number" onClick={() => handleClick(1)}>
-                            <button className="number-button">
-                                1
+                            <button className="button-style" onClick={navigateHome}>
+                                Visszalépés
                             </button>
-                        </div>
-                        <div className="number" onClick={() => handleClick(2)}>
-                            <button className="number-button">
-                                2
-                            </button>
-                        </div>
-                        <div className="number" onClick={() => handleClick(3)}>
-                            <button className="number-button">
-                                3
-                            </button>
-                        </div>
-                        <div className="number" onClick={() => handleClick(4)}>
-                            <button className="number-button">
-                                4
-                            </button>
-                        </div>
-                        <div className="number" onClick={() => handleClick(5)}>
-                            <button className="number-button">
-                                5
-                            </button>
-                        </div>
-                        <div className="number" onClick={() => handleClick(6)}>
-                            <button className="number-button">
-                                6
-                            </button>
-                        </div>
-                        <div className="number" onClick={() => handleClick(7)}>
-                            <button className="number-button">
-                                7
-                            </button>
-                        </div>
-                        <div className="number" onClick={() => handleClick(8)}>
-                            <button className="number-button">
-                                8
-                            </button>
-                        </div>
-                        <div className="number" onClick={() => handleClick(9)}>
-                            <button className="number-button">
-                                9
-                            </button>
-                        </div>
-                        <div className="number"  onClick={() => handleClick(1)}>
-                            <button className="number-button">
-                                0
-                            </button>
-                        </div>
-                        <div className="idk">
-                            <button className="idk-button" onClick={() => handleClick("Nem tudom")}>
-                                Nem tudom
+                            <button className="button-style right-button" onClick={navigateLeaderboard}>
+                                Toplista
                             </button>
                         </div>
                     </div>
-                    <button className="button-style" onClick={navigateHome}>
-                        Visszalépés
-                    </button>
-                    <button className="button-style right-button" onClick={navigateLeaderboard}>
-                        Toplista
-                    </button>
                 </div>
             </div>
         </div>
