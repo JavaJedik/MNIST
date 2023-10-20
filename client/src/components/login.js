ef0c40c1fb287ea7d5777f4b79297dfe178ec626
Veres Viktor Árpád <veresviktor02@gmail.com>
2023-10-20 16:06:30 +0200

    darkmode implementálása

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -6,6 +6,7 @@ import AuthService from '../AuthService';
 const Login = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [darkMode, setDarkMode] = useState(false);
     const navigate = useNavigate();
 
     const navigateHome = async () => {
@@ -31,11 +32,24 @@ const Login = () => {
 
     return (
         <div className="main-container">
-            <div className="blur-container" />
+
+            <label className="switch">
+                <input
+                    defaultChecked={false}
+                    type="checkbox"
+                    checked={darkMode}
+                    onChange={() => setDarkMode(!darkMode)}
+                />
+                    <span className="slider round"></span>
+            </label>
+
+            <div className={`classname ${darkMode ? "dark" : ""}`} />
+
+            <div className={`blur-container ${darkMode ? "dark-blur-container" : ""}`} />
             <div className="container">
                 <div className="content">
                     <input
-                        className="input-field"
+                        className={`input-field ${darkMode ? "dark-input-field" : ""}`}
                         type="text"
                         placeholder="Felhasználónév"
                         id="username"
@@ -45,7 +59,7 @@ const Login = () => {
                 </div>
                 <div className="content">
                     <input
-                        className="input-field"
+                        className={`input-field ${darkMode ? "dark-input-field" : ""}`}
                         type="password"
                         placeholder="Jelszó"
                         id="password"
@@ -54,15 +68,23 @@ const Login = () => {
                     />
                 </div>
                 <div className="content">
-                    <button className="button-style" onClick={navigateHome}>
+                    <button
+                        className="button-style"
+                        className={`button-style ${darkMode ? "dark-button-style" : ""}`}
+                        onClick={navigateHome}
+                    >
                         Bejelentkezés
                     </button>
-                    <button className="button-style" onClick={navigateRegister}>
+                    <button
+                        className="button-style"
+                        className={`button-style ${darkMode ? "dark-button-style" : ""}`}
+                        onClick={navigateRegister}
+                    >
                         Regisztráció
                     </button>
                 </div>
                 <div>
-                    <p className="content-bottom">
+                    <p className={`content-bottom ${darkMode ? "dark-content-bottom" : ""}`}>
                         Jelentkezz be, vagy regisztrálj a kezdéshez!
                     </p>
                 </div>
