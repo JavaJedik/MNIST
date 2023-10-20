05915f68aed737dae2702e0ae5b703931ce183dd
Menyhárt Tamás <tomi04252002@gmail.com>
2023-10-20 18:56:04 +0200

    CORS beállítások

--- client/src/AuthService.js
+++ client/src/AuthService.js
@@ -2,26 +2,42 @@ const API_URL = 'https://mnist-server.serveo.net';
 
 const AuthService = {
     
-  login: async (username, password) => {
+    login: async (username, password) => {
     try {
+      // Elküldjük a kérést és kiírjuk a küldött adatokat
+      const requestData = {
+        username,
+        password
+      };
+      console.log('Küldött adatok:', requestData);
+
       const response = await fetch(`${API_URL}/login`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify({
-          username,
-          password
-        })
+        body: JSON.stringify(requestData)
       });
 
-      const data = await response.json();
+      if (response.status === 200) {
+        // Ha a státuszkód 200 OK, olvassuk el a választ szövegként
+        const responseText = await response.text();
+        console.log('Válasz típusa:', typeof responseText);
+        console.log('Válasz tartalma:', responseText);
 
-      if (data.success) {
-        localStorage.setItem('userToken', data.userToken);
+        // Adjunk vissza egy JSON objektumot a válaszból
+        return { success: true, userToken: responseText };
+      } else if (response.status === 401) {
+        // Ha a státuszkód 401 Unauthorized, nincs válasz
+        console.log('Nincs válasz a státuszkód 401 esetén.');
+
+        // Adjunk vissza egy JSON objektumot a válaszból
+        return { success: false, userToken: '' };
+      } else {
+        // Egyéb státuszkódok esetén
+        console.log('Nem kezelt státuszkód:', response.status);
+        throw new Error('Login failed');
       }
-
-      return data;
     } catch (error) {
       console.error('Fetch error:', error);
       throw new Error('Login failed');
