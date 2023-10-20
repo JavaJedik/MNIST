4b558d07820fb75e0458e26695a8dfadfe4ed544
Pankotai Kristóf Géza <p.kristof1999@gmail.com>
2023-10-20 12:13:12 +0200

    navigateHome átírása a login frontenden

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import "./styles.css"
-//import AuthService from '../AuthService';
+import AuthService from '../AuthService';
 
 const Login = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const navigate = useNavigate();
 
-    /*const navigateHome = async () => {
+    const navigateHome = async () => {
         try {
             const data = await AuthService.login(username, password);
 
@@ -20,10 +20,6 @@ const Login = () => {
         } catch (error) {
             console.error('Login failed:', error);
         }
-    };*/
-
-    const navigateHome = () => {
-        navigate('/home');
     };
 
     const navigateRegister = () => {
@@ -40,7 +36,7 @@ const Login = () => {
                         type="text"
                         placeholder="Felhasználónév"
                         id="username"
-                        //value={username}
+                        value={username}
                     />
                 </div>
                 <div className="content">
@@ -49,7 +45,7 @@ const Login = () => {
                         type="password"
                         placeholder="Jelszó"
                         id="password"
-                        //value={password}
+                        value={password}
                     />
                 </div>
                 <div className="content">
