403c135b69383b6981616d9fae6831bb6913fddb
Pankotai Kristóf Géza <p.kristof1999@gmail.com>
2023-10-21 13:33:51 +0200

    registerUser létrehozása az AuthService fájlban.

--- client/src/AuthService.js
+++ client/src/AuthService.js
@@ -1,8 +1,8 @@
 const API_URL = 'http://localhost:8814'; //'https://mnist-server.serveo.net';
 
 const AuthService = {
-    
-    login: async (username, password) => {
+
+  login: async (username, password) => {
     try {
       // Elküldjük a kérést és kiírjuk a küldött adatokat
       const requestData = {
@@ -26,13 +26,13 @@ const AuthService = {
         console.log('Válasz tartalma:', responseText);
 
         // Adjunk vissza egy JSON objektumot a válaszból
-        return { success: true, userToken: responseText };
+        return {success: true, userToken: responseText};
       } else if (response.status === 401) {
         // Ha a státuszkód 401 Unauthorized, nincs válasz
         console.log('Nincs válasz a státuszkód 401 esetén.');
 
         // Adjunk vissza egy JSON objektumot a válaszból
-        return { success: false, userToken: '' };
+        return {success: false, userToken: ''};
       } else {
         // Egyéb státuszkódok esetén
         console.log('Nem kezelt státuszkód:', response.status);
@@ -43,11 +43,10 @@ const AuthService = {
       throw new Error('Login failed');
     }
   },
-  
-  
+
   checkLoggedIn: async () => {
     const userToken = localStorage.getItem('userToken');
-    
+
     try {
       const response = await fetch(`${API_URL}/check-logged-in`, {
         method: 'POST',
@@ -64,7 +63,47 @@ const AuthService = {
       console.error('Fetch error:', error);
       throw new Error('Token check failed');
     }
+  },
+
+  registerUser: async (username, email, password, language, gender, country) => {
+    try {
+      const requestData = {
+        username,
+        email,
+        password,
+        language,
+        gender,
+        country
+      };
+
+      console.log('Küldött adatok:', requestData);
+
+      const response = await fetch(`${API_URL}/registrate`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(requestData)
+      });
+
+      if (response.status === 200) {
+        const responseText = await response.text();
+        console.log('Válasz típusa:', typeof responseText);
+        console.log('Válasz tartalma:', responseText);
+
+        return { success: true, userToken: responseText };
+      } else if (response.status === 409) {
+        console.log('Nincs válasz a státuszkód 409 esetén.');
+
+        return { success: false, userToken: '' };
+      } else {
+        console.log('Nem kezelt státuszkód:', response.status);
+        throw new Error('Login failed');
+      }
+    } catch (error) {
+      console.error('Fetch error:', error);
+      throw new Error('Login failed');
+    }
   }
 };
-
 export default AuthService;
