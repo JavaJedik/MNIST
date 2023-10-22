979307924612aac2abb338bf0a2a6420e24a9e22
Veres Viktor Árpád <veresviktor02@gmail.com>
2023-10-22 14:20:39 +0200

    picture_game hozzáadva route-hoz

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Home from './components/home';
 import Register from './components/register';
 import Leaderboard from './components/leaderboard';
 import Number_Game from './components/number_game';
+import Picture_Game from './components/picture_game';
 import Admin from './components/admin';
 import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 function App() {
@@ -17,6 +18,7 @@ function App() {
             <Route path="/register" element={<Register />} />
             <Route path="/leaderboard" element={<Leaderboard />} />
             <Route path="/number_game" element={<Number_Game />} />
+            <Route path="/picture_game" element={<Picture_Game />} />
             <Route path="/admin" element={<Admin />} />
         </Routes>
       </BrowserRouter>
