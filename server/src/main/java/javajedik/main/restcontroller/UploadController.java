2680331857637aa2d3dfc99ede880d5839a7ba79
EgyipTomi425 <tomi04252002@gmail.com>
2023-10-28 21:50:59 +0200

    Kép binárissá alakítása, adatok eltávolítása, átkonvertálása png-be

---
+++ server/src/main/java/javajedik/main/restcontroller/UploadController.java
@@ -0,0 +1,41 @@
+package javajedik.main.restcontroller;
+
+import javajedik.main.util.ImageUtil;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/upload")
+public class UploadController 
+{
+    private static final Logger logger = LogManager.getLogger(UploadController.class);
+    
+    @PostMapping("/picture")
+    public ResponseEntity<String> uploadPicture(@RequestBody byte[] binaryData)
+    {              
+        long fileSizeInBytes = binaryData.length;
+        long fileSizeInMB = fileSizeInBytes / (1024 * 1024); // Byte-okból Megabyte-okká
+        if (fileSizeInMB > 100) 
+        {
+            logger.warn("Túl nagy a kép méréte, http 413 küldése...");
+            return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE).body("A kép mérete meghaladja a 100 MB-ot.");
+        }
+        
+        binaryData = ImageUtil.convertToPNG(binaryData);
+        
+        if(binaryData == null)
+        {
+            logger.warn("A kép biztonságos feldolgozása sikertelen, http 406 küldése...");
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body("A kép biztonságos feldolgozása sikertelen.");
+        }
+
+        
+        return null;
+    }
+}
