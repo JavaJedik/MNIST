e6fc56bbcd3f27a9395cd4b32c348919ae6b57a9
EgyipTomi425 <88924024+EgyipTomi425@users.noreply.github.com>
2024-02-12 14:22:29 +0100

    Update AuthService.js

--- client/src/AuthService.js
+++ client/src/AuthService.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://mnist-server.serveo.net';
+const API_URL = 'http://localhost:8814';
 /*'https://1c544cddb7c3cfcf4e845eadf5c5ee81.serveo.net';
 'https://mnist-server.serveo.net';*/
 
